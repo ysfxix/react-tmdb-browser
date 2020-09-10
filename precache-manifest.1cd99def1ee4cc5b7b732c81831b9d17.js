d85c40f7501d1e3acb5f3abddcde2ce53be8c25e
yusuf-alii <ysf.ali@gmail.com>
2020-09-10 13:15:28 +0530

    Updates

---
+++ precache-manifest.1cd99def1ee4cc5b7b732c81831b9d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3b755ce38b5366ee32b",
+    "url": "/react-tmdb-browser/static/js/runtime~main.b3b755ce.js"
+  },
+  {
+    "revision": "98ce6058bd59d50fd30c",
+    "url": "/react-tmdb-browser/static/js/main.98ce6058.chunk.js"
+  },
+  {
+    "revision": "e1db4c2685bdb8441d44",
+    "url": "/react-tmdb-browser/static/js/1.e1db4c26.chunk.js"
+  },
+  {
+    "revision": "98ce6058bd59d50fd30c",
+    "url": "/react-tmdb-browser/static/css/main.367ea891.chunk.css"
+  },
+  {
+    "revision": "bcecff06e7c80c14980e75595eb5949b",
+    "url": "/react-tmdb-browser/index.html"
+  }
+];
